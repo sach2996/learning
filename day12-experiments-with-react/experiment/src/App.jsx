9512d96fbb31754f9fbf86aa5a35ed193a68fe28
Sachin Sharma <sachinsharma@Sachins-MacBook-Pro.local>
2024-02-09 20:04:04 -0500

    useEffect hook use learnt

--- day12-experiments-with-react/experiment/src/App.jsx
+++ day12-experiments-with-react/experiment/src/App.jsx
@@ -1,27 +1,40 @@
+import { useEffect } from "react";
 import { useState } from "react";
 function App() {
+  const [todo, setTodo] = useState([]);
+
+  //useEffect is to define function and dependency array(if empty then it runs on component mount)
+  //we can not use async function in useEffect
+  useEffect(() => {
+    setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos").then(async (res) => {
+        const jsonRes = await res.json();
+        console.log("res", jsonRes.todos);
+        setTodo(jsonRes.todos);
+        console.log(todo);
+      });
+    }, 5000);
+  }, []);
   return (
     <>
-      <HeaderWithButton />
-      <Header title="name" />
+      {todo.map((val) => (
+        <TodoComponent
+          key={val.id}
+          title={val.title}
+          description={val.description}
+        />
+      ))}
     </>
   );
 }
 
-function HeaderWithButton() {
-  const [title, setTitle] = useState("test");
-  function onClickHandler() {
-    setTitle(Math.random);
-  }
+function TodoComponent({ title, description }) {
+  console.log({ title });
   return (
     <>
-      <button onClick={onClickHandler}>Generate new title </button>
-      <Header title={title} />
+      <h2>{title}</h2>
+      <div>{description}</div>
     </>
   );
-}
-
-function Header(props) {
-  return <div>My name is {props.title}</div>;
 }
 export default App;
